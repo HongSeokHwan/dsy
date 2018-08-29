8117f60e7febdaf964f9e111aa3937ef3759c500
huffonism@gmail.com <huffonism@gmail.com>
2018-08-29 15:41:26 +0900

    sched

--- heo/week3/scheduler/scheduler.py
+++ heo/week3/scheduler/scheduler.py
@@ -1,33 +1,67 @@
-#from .. import priority_queue as pq
+import priority_queue as pq
 
 class Scheduler:
-
+    # Constructor
     def __init__(self):
-        #self.priority_queue = pq.PriorityQueueHeap()
-        print("Welcome to devfon's scheduler")
-        self.status = None
-        pass
+        self.queue = pq.PriorityQueueHeap()
 
     def prompt(self):
-        command = input('>>> ')
-        return command
+        command = 'add 10:20 job'
+        self.parser(command)
+        command = 'add 9:20 alarm'
+        self.parser(command)
+        command = 'add 7:20 method'
+        self.parser(command)
+        command = 'add 5:20 method'
+        self.parser(command)
+        command = 'list'
+        self.parser(command)
 
-    def parser(self):
-        pass
+    # Parsing the command
+    def parser(self, command):
+        command_tokens = command.split(' ')
 
-    def add(self):
-        pass
+        if command_tokens[0] == 'add':
+            self.add(command_tokens[1], command_tokens[2])
+        elif command_tokens[0] == 'list':
+            self.list()
+
+    # Add the job to the scheduler
+    def add(self, time, job_name):
+        time = int(time.replace(':', ''))
+        self.queue.insert(time, job_name)
+
+    # Show pending jobs
+    def list(self):
+        # print('[Job Scheduler]\n')
+        print('Rank\t     Job')
+        new_queue = pq.PriorityQueueHeap()
+        new_queue.heap = self.queue.heap[:]
+        new_queue.size = self.queue.size
+        th = 1
+        while(1):
+            if new_queue.size == 0:
+                break
+            showing_job = new_queue.delete()
+            str_time = str(showing_job[0])
+            str_time = str_time[:-2] + ':' + str_time[-2:]
+            print(' ', th, '\t', str_time, showing_job[1])
+            th += 1
 
     def time_check(self):
         pass
 
-    def list(self):
+    def run(self):
         pass
 
 if __name__ == '__main__':
     sched = Scheduler()
-    newline = sched.prompt()
-    print(newline)
+    sched.prompt()
+    # print(sched.queue.delete())
+    # print(sched.queue.delete())
+    # print(sched.queue.delete())
+    # print(sched.queue.delete())
+    # print(sched.queue.delete())
     # while(1):
     #     newline = sched.prompt()
     #     print(newline)
---
+++ heo/week3/scheduler/test_scheduler.py
@@ -0,0 +1,10 @@
+import scheduler as sc
+
+if __name__ == '__main__':
+    sched = sc.Scheduler()
+    newline = sched.prompt()
+    # while(1):
+    #     newline = sched.prompt()
+    #     print(newline)
+    #     if newline == 'exit\n':
+    #         break
